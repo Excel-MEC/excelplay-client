cae1351223f7505da22f0c4badfc76978ee6bb38
Prince Mathew <i.princemathew@gmail.com>
2018-10-18 23:16:36 +0530

    Update

--- src/app/components/kryptos/kryptos-play/kryptos-play.component.ts
+++ src/app/components/kryptos/kryptos-play/kryptos-play.component.ts
@@ -44,7 +44,7 @@ export class KryptosPlayComponent implements OnInit {
       .subscribe(level => {
         this.level = level;
         this.loadUserRank();
-        if (level == 5)
+        if (level.level == 5)
           console.log("an");
       });
   }
